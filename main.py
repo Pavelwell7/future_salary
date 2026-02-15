a7fc98d9b2e3710e84979f7bd8cf72913f13c251
Pavelwell7 <pavelbugaev303@gmail.com>
2026-02-15 17:13:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,38 @@
 import requests
+from itertools import count
 
 from environs import Env
 from terminaltables import AsciiTable
-from itertools import count
 
+
+def predict_rub_salary(salary_from, salary_to):
+    if not salary_from and not salary_to:
+        return None
+    if salary_from and salary_to:
+        return (salary_from + salary_to) / 2
+    elif salary_from:
+        return salary_from * 1.2
+    elif salary_to:
+        return salary_to * 0.8
 
 def predict_rub_salary_hh(vacancy):
     if  vacancy['salary'] is None or vacancy['salary']['currency'] != 'RUR':
         return None
-    if vacancy['salary']['from'] and vacancy['salary']['to']:
-        return  (vacancy['salary']['to'] + vacancy['salary']['from'])/2
-    elif vacancy['salary']['from']:
-        return vacancy['salary']['from'] * 1.2
-    elif vacancy['salary']['to']:
-        return vacancy['salary']['to'] * 0.8
-    return None
+    salary_from = vacancy['salary']['from']
+    salary_to = vacancy['salary']['to']
+    return predict_rub_salary(salary_from, salary_to)
 
-def get_language_statistic_hh(url, language):
+def get_language_statistic_hh(language):
+
+    url_hh = 'https://api.hh.ru/vacancies'
+
     search_field = f'Программист {language}'
+    moscow = 1
+    pages = 100
     payload = {
         'text': search_field,
-        'area': 1,
-        'per_page': 100,
+        'area': moscow,
+        'per_page': pages,
     }
     headers = {
         'User-Agent': 'curl/7.74.0',
@@ -32,7 +43,7 @@ def get_language_statistic_hh(url, language):
     total_salary = 0
     for page in count(0):
         payload['page'] = page
-        page_response = requests.get(url, params=payload, headers=headers)
+        page_response = requests.get(url_hh, params=payload, headers=headers)
         page_response.raise_for_status()
         vacancies = page_response.json()
         if page == 0:
@@ -44,7 +55,7 @@ def get_language_statistic_hh(url, language):
                 vacancies_processed += 1
         if page >= vacancies['pages']:
             break
-    if vacancies_processed>0:
+    if vacancies_processed:
         average_salary = int(total_salary / vacancies_processed)
     else:
         average_salary = 0
@@ -59,23 +70,17 @@ def predict_rub_salary_sj(vacancy):
         return None
     salary_from = vacancy['payment_from']
     salary_to = vacancy['payment_to']
-    if not salary_from and not salary_to:
-        return None
-    if salary_from and salary_to:
-        return (salary_from + salary_to) / 2
-    elif salary_from:
-        return salary_from * 1.2
-    elif salary_to:
-        return salary_to * 0.8
+    return predict_rub_salary(salary_from, salary_to)
 
-    return None
-
-def get_language_statistic_sj(url, language, token):
+def get_language_statistic_sj(language, token):
+    url_sj = 'https://api.superjob.ru/2.0/vacancies'
     search_field = f'Программист {language}'
+    moscow = 4
+    pages = 100
     payload = {
         'keyword': search_field,
-        'town': 4,
-        'count': 100,
+        'town': moscow,
+        'count': pages,
         'page': 0,
     }
     headers = {
@@ -88,7 +93,7 @@ def get_language_statistic_sj(url, language, token):
     total_salary = 0
     for page in count(0):
         payload['page'] = page
-        page_response = requests.get(url, params=payload, headers=headers)
+        page_response = requests.get(url_sj, params=payload, headers=headers)
         page_response.raise_for_status()
         vacancies = page_response.json()
         if page == 0:
@@ -100,7 +105,7 @@ def get_language_statistic_sj(url, language, token):
                 vacancies_processed += 1
         if page >= vacancies['more']:
             break
-    if vacancies_processed>0:
+    if vacancies_processed:
         average_salary = int(total_salary / vacancies_processed)
     else:
         average_salary = 0
@@ -110,15 +115,7 @@ def get_language_statistic_sj(url, language, token):
         'average_salary': average_salary,
     }
 
-def main():
-
-    env = Env()
-    env.read_env()
-
-    url_hh = env('HH_URL')
-    url_sj = env('SJ_URL')
-    token = env('TOKEN')
-
+def print_statistics_table(title, stats_function):
     languages = [
         'Python',
         'SQL',
@@ -130,41 +127,34 @@ def main():
         'c++',
         'go',
     ]
-    table_hh = [[
+    table = [[
         'Язык программирования',
         'Вакансий найдено',
         'Вакансий обработано',
         'Средняя зарплата'
     ]]
     for lang in languages:
-        stats_hh = get_language_statistic_hh(url_hh, lang)
-        table_hh.append([
-            lang,
-            stats_hh['vacancies_found'],
-            stats_hh['vacancies_processed'],
-            stats_hh['average_salary'],
-        ])
-    table_sj = [[
-        'Язык программирования',
-        'Вакансий найдено',
-        'Вакансий обработано',
-        'Средняя зарплата',
-    ]]
-    table_ascii_hh = AsciiTable(table_hh)
-    table_ascii_hh.title = 'HeadHunter Moscow'
-    print(table_ascii_hh.table)
-
-    for lang in languages:
-        stats = get_language_statistic_sj(url_sj, lang, token)
-        table_sj.append([
+        stats = stats_function(lang)
+        table.append([
             lang,
             stats['vacancies_found'],
             stats['vacancies_processed'],
             stats['average_salary'],
         ])
-    table_ascii_sj = AsciiTable(table_sj)
-    table_ascii_sj.title = 'SuperJob Moscow'
-    print(table_ascii_sj.table)
+    ascii_table = AsciiTable(table)
+    ascii_table.title = title
+    print(ascii_table.table)
+
+def main():
+
+    env = Env()
+    env.read_env()
+
+    token_sj = env('TOKEN_SJ')
+
+    print_statistics_table('HeadHunter Moscow',  get_language_statistic_hh)
+    print_statistics_table('SuperJob Moscow', lambda lang: get_language_statistic_sj(lang, token_sj))
+
 
 if __name__ == '__main__':
     main()
